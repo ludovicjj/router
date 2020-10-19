0b022368be0794fef2a392cb206710ed1330ef5f
ludovicjj <jahanlud@gmail.com>
2020-10-19 12:25:16 +0200

    Init RouteCollection and get route

--- src/Router.php
+++ src/Router.php
@@ -6,5 +6,41 @@ namespace App;
 
 class Router
 {
+    /**
+     * @var Route[]
+     */
+    private $routes = [];
 
+    /**
+     * @return Route[]
+     */
+    public function getRouteCollection(): array
+    {
+        return $this->routes;
+    }
+
+    /**
+     * @param Route $route
+     * @return $this
+     */
+    public function add(Route $route): self
+    {
+        $this->routes[$route->getName()] = $route;
+
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return Route|null
+     */
+    public function get(string $name): ?Route
+    {
+        foreach ($this->routes as $route) {
+            if ($route->getName() === $name) {
+                return $route;
+            }
+        }
+        return null;
+    }
 }

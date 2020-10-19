2a3dbf87742eff557e551e8c73c87285f9346688
ludovicjj <jahanlud@gmail.com>
2020-10-19 12:25:52 +0200

    Init Route

---
+++ src/Route.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App;
+
+
+class Route
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var callable $callable
+     */
+    private $callable;
+
+    public function __construct(
+        string $name,
+        string $path,
+        $callable
+    ) {
+        $this->name = $name;
+        $this->path = $path;
+        $this->callable = $callable;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
